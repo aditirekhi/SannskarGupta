b765a42ac1da16b41f3356322d785aadaa663bb1
arekhi <aditirekhi99@gmail.com>
2024-09-27 22:28:17 +0530

    Made changes for constants

--- src/app/constants/constants.service.ts
+++ src/app/constants/constants.service.ts
@@ -6,6 +6,8 @@ import { Injectable } from '@angular/core';
 export class ConstantsService {
   public readonly APP_TITLE: string = 'Sannskar Gupta';
   public readonly PORTFOLIO_ALT_TEXT: string = 'Portfolio Image';
+
+  // Tab name constants
   public readonly HOME_TAB: string = 'Home';
   public readonly EXPERIENCE_TAB: string = 'Experience';
   public readonly EDUCATION_TECHNICAL_SKILLS_TAB: string = "EducationAndTechnicalSkills";
